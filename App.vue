b437b1472b75c9f2cd794345c656dd34221e56c8
leegyuseung <lgs4002@naver.com>
2022-10-20 23:09:08 +0900

    compositionAPI, setup

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>{{ counter }}</p>
+  <p>{{ message }}</p>
+  <button @click="increment">+button</button>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const counter = ref(0);
+    const message = ref("hello vue3");
+    const increment = () => {
+      counter.value++;
+    };
+
+    return { counter, message, increment };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/lectures/4/App_CompositionAPI.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { isRef, onBeforeMount, onMounted, ref } from "vue";
+
+const reactiveMessage = ref("hello Reactive Message");
+const addReactiveMessage = () => {
+  reactiveMessage.value = reactiveMessage.value + "!";
+};
+console.log(isRef(reactiveMessage)); //isRef : 반응형인지 확인한다.
+
+let normalMessage = "Normal Message";
+const addNormalMessage = () => {
+  normalMessage = normalMessage + "!";
+};
+console.log(isRef(addNormalMessage));
+
+// 라이프 사이클 훅
+onMounted(() => {
+  console.log("onMounted");
+});
+onBeforeMount(() => {
+  console.log("onBeforeMount");
+});
+</script>
+
+<template>
+  <h1>반응형 메지지</h1>
+  <p>{{ reactiveMessage }}</p>
+  <button @click="addReactiveMessage">add message</button>
+
+  <h1>일반 메지지</h1>
+  <p>{{ normalMessage }}</p>
+  <button @click="addNormalMessage">add message</button>
+</template>
+
+<style scoped></style>
