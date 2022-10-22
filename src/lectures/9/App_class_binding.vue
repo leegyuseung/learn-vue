4d2c5959f4dae79ec2975808f4879596cd8196de
leegyuseung <lgs4002@naver.com>
2022-10-22 23:54:38 +0900

    class 와 style 바인딩

---
+++ src/lectures/9/App_class_binding.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <!-- <div class="text" :class="classObject">텍스트 입니다.</div> -->
+    <div class="text" :class="[activeClass, errorClass]">텍스트 입니다.</div>
+    <!-- <div
+      class="text"
+      :class="[isActive ? 'active-class' : 'class', errorClass, classObject]"
+    ></div> -->
+
+    <button @click="toggle">toggle</button>
+    <button @click="hasError = !hasError">toggleError</button>
+  </div>
+</template>
+
+<script>
+import { computed, reactive, ref } from "vue";
+
+export default {
+  setup() {
+    const isActive = ref(true);
+    const hasError = ref(false);
+
+    // const classObject = reactive({
+    //   active: true,
+    //   "text-danger": false,
+    // });
+
+    const classObject = computed(() => {
+      return {
+        active: true && true,
+        "text-danger": true && true,
+        "text-blue": true,
+      };
+    });
+
+    const activeClass = ref("active");
+    const errorClass = ref("error");
+
+    const toggle = () => {
+      isActive.value = !isActive.value;
+    };
+    return { isActive, toggle, hasError, classObject, errorClass, activeClass };
+  },
+};
+</script>
+
+<style>
+.active {
+  font-weight: 900;
+}
+.text-danger {
+  color: red;
+}
+</style>
---
+++ src/lectures/9/App_style_binding.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :style="styleObject">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto est
+    cupiditate consequatur. Culpa, eaque enim repudiandae inventore magnam
+    repellendus. Nobis tempora voluptas fuga deserunt ipsam. Soluta fugit
+    assumenda sed nam.
+  </div>
+  <button @click="fontSize--">-</button>
+  <button @click="fontSize++">+</button>
+</template>
+
+<script>
+import { computed, reactive, ref } from "vue";
+
+export default {
+  setup() {
+    // const styleObject = reactive({
+    //   color: "red",
+    //   fontSize: "13px",
+    // });
+    const fontSize = ref(13);
+    const styleObject = computed(() => {
+      return {
+        color: "red",
+        fontSize: fontSize.value + "px",
+      };
+    });
+    return { styleObject, fontSize };
+  },
+};
+</script>
+
+<style></style>
