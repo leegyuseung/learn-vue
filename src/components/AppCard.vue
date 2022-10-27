627ad5afa189f4b936f226ca67bdb191e4fc83f3
leegyuseung <lgs4002@naver.com>
2022-10-27 22:50:08 +0900

    Props

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,28 +1,66 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <span class="badge bg-secondary">{{ typeName }}</span>
+      <h5 class="card-title mt-2">{{ title }}</h5>
       <!-- <p class="card-text" :class="$style.red"> -->
       <p class="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
+        {{ contents }}
       </p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <a href="#" class="btn" :class="isLikeClass">좋아요</a>
+      <br />
+      {{ obj }}
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { computed, ref } from "vue";
 console.log("appcard module");
 export default {
-  setup() {
+  props: {
+    type: {
+      type: String,
+      default: "news",
+      validator: (value) => {
+        return ["news", "notice"].includes(value);
+      },
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    contents: {
+      type: String,
+      required: true,
+    },
+    isLike: {
+      type: Boolean,
+      default: false,
+    },
+    obj: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  emits: ["toggleLike"],
+  setup(props, context) {
+    console.log("props.title", props.title);
+    const isLikeClass = computed(() =>
+      props.isLike ? "btn-danger" : "btn-outline-danger"
+    );
+    const typeName = computed(() =>
+      props.type === "news" ? "뉴스" : "공지사항"
+    );
+    const toggleLike = () => {
+      context.emit("toggleLike");
+    };
     // const style = useCssModule();
     // console.log(style);
     const color = ref("red");
     console.log("appcard setup");
     color.value = "yellow";
-    return { color };
+    return { color, isLikeClass, typeName, toggleLike };
   },
 };
 </script>
