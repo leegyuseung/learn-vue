8fda9f44a4b0e9d557cadc0a1f25ee69d98b3eff
leegyuseung <lgs4002@naver.com>
2022-10-27 21:56:54 +0900

    컴포넌트 기초

--- App.vue
+++ App.vue
@@ -1,9 +1,20 @@
 <template>
-  <div></div>
+  <div>
+    <TheNav></TheNav>
+    <TheVue></TheVue>
+    <AppCard></AppCard>
+  </div>
 </template>
 
 <script>
-export default { setup() {} };
+import AppCard from "./src/components/AppCard.vue";
+import TheNav from "./src/components/TheNav.vue";
+import TheVue from "./src/components/TheVue.vue";
+
+export default {
+  components: { TheVue, TheNav, AppCard },
+  setup() {},
+};
 </script>
 
 <style></style>
---
+++ src/components/TheVue.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <div class="container py-4">
+      <div class="row g-3">
+        <div class="col-4"><AppCardVue /></div>
+        <div class="col-4"><AppCardVue /></div>
+        <div class="col-4"><AppCardVue /></div>
+        <div class="col-4"><AppCardVue /></div>
+        <div class="col-4"><AppCardVue /></div>
+        <div class="col-4"><AppCardVue /></div>
+
+        <!-- <div class="col-4"><app-card-vue /></div> -->
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import AppCardVue from "./AppCard.vue";
+export default {
+  components: {
+    AppCardVue,
+  },
+};
+</script>
+
+<style></style>
