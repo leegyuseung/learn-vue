42b08f92ad7b5d512096a5b5327567c77f7f8c43
leegyuseung <lgs4002@naver.com>
2022-10-30 21:57:12 +0900

    Provide/Inject

--- src/components/TheVue.vue
+++ src/components/TheVue.vue
@@ -1,59 +1,13 @@
 <template>
   <main>
-    <div class="container py-4">
-      <!-- @click="sayHello" -->
-      <MyButton class="my-button" id="my-button" @click="sayHello"></MyButton>
-      <LabelInput label="이름" data-id="id입니다." />
-      <hr />
-      <FancyButton>Click!!!<span style="color: red">@@@</span></FancyButton>
-      <!-- <FancyButton v-slot="{ fancyMessage }">{{ fancyMessage }}</FancyButton> -->
-      <FancyButton
-        ><template v-slot="{ fancyMessage }">{{
-          fancyMessage
-        }}</template></FancyButton
-      >
-
-      <hr />
-      <AppCard>
-        <!-- v-on v-bind v-slot 은 동적으로 가능하다. -->
-        <!-- <template #[slotArgs]> 제목입니다. </template> -->
-        <!-- <template #default> 내용입니다. </template> -->
-        <!-- 암시적으로 default 슬롯입니다.! -->
-        <!-- <template #footer> 푸터입니다. </template> -->
-
-        <template #default="{ childMessage, helloMessage }"
-          >default입니다. {{ message }} {{ childMessage }}{{ helloMessage }}
-        </template>
-        <template #header="{ headerMessage }"
-          >default입니다. {{ headerMessage }}
-        </template>
-        <template #footer="{ footerMessage }"
-          >default입니다. {{ footerMessage }}
-        </template>
-      </AppCard>
-      <AppCard>게시글입니다.</AppCard>
-    </div>
+    <ProvideInject></ProvideInject>
   </main>
 </template>
 
 <script>
-import { ref } from "vue";
-import AppCard from "./AppCard.vue";
-import FancyButton from "./FancyButton.vue";
-import LabelInput from "./LabelInput.vue";
-import MyButton from "./MyButton.vue";
+import ProvideInject from "./ProvideInject.vue";
 
-export default {
-  components: { MyButton, LabelInput, FancyButton, AppCard },
-  setup() {
-    const sayHello = () => {
-      alert("안녕");
-    };
-    const slotArgs = ref("header");
-    const message = ref("Hi");
-    return { sayHello, slotArgs, message };
-  },
-};
+export default { components: { ProvideInject }, setup() {} };
 </script>
 
 <style></style>
--- src/main.js
+++ src/main.js
@@ -6,5 +6,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const app = createApp(App);
 // app.component("AppCard", AppCard);
 
+app.provide("app-message", "app message 입니다.");
+//
+app.config.globalProperties.msg = "hello";
 app.mount("#app");
 import "bootstrap/dist/js/bootstrap.js";
